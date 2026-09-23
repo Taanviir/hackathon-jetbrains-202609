19d45dc02b545693f86704c3a67dac7e73469006
aikram42 <153020650+aikram42@users.noreply.github.com>
2026-09-23 11:04:32 +0400

    build: align IntelliJev with IDE 2025.2

--- plugins/intellijev/build.gradle.kts
+++ plugins/intellijev/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
     kotlin("jvm") version "2.1.20"
-    id("org.jetbrains.intellij.platform") version "2.3.0"
+    id("org.jetbrains.intellij.platform") version "2.16.0"
 }
 
 group = "dev.intellijev"
